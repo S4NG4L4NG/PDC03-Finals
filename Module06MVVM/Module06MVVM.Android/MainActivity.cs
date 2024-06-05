a10bf3d499adfa00eb1a4d631d60f034f119cf4e
Lean <sangalng.leandro@auf.edu.ph>
2024-06-05 18:08:25 +0800

    This is a commit

--- Module06MVVM/Module06MVVM.Android/MainActivity.cs
+++ Module06MVVM/Module06MVVM.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace Module06MVVM.Droid
 {
-    [Activity(Label = "Module06MVVM", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Gamia", Icon = "@drawable/logo", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
